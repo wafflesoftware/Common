3f08e6f3148d2de08bb5fd299fbce38027b622c4
Jesper <wootest@gmail.com>
2011-03-06 22:36:07 +0100

    WFSparseTimer - run an imprecise recurring timer that only fires when you signal activity. Good for updating status information.

---
+++ WFSparseTimer.h
@@ -0,0 +1,33 @@
+//
+//  WFSparseTimer.h
+//
+//  Created by Jesper on 2011-03-06.
+//
+//  The contents of this file is placed in the public domain.
+//  No rights are reserved, asserted or exercised.
+//  For more information, see:
+//    http://creativecommons.org/publicdomain/zero/1.0/
+//
+
+#import <Foundation/Foundation.h>
+#import <dispatch/dispatch.h>
+
+@interface WFSparseTimer : NSObject {
+@private
+    dispatch_queue_t controlQueue;
+    dispatch_queue_t blockQueue;
+    dispatch_source_t source;
+    dispatch_block_t block;
+    BOOL shouldFire;
+    BOOL tossed;
+}
+- (id)initWithDispatchQueue:(dispatch_queue_t)queue
+            atRoughInterval:(NSTimeInterval)interval
+                      block:(dispatch_block_t)block;
+
+- (void)setNeedsFire;
+
+// invalidate will, in accordance with NSTimer precedence, release the timer
+- (void)invalidate;
+- (BOOL)isValid;
+@end
